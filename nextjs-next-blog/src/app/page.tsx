3dff8cd14bff8ebd65816442a7d2eba512db0c72
Md Ataullah <ataullahm100@gmail.com>
2024-11-09 01:58:55 +0600

    Fixed Shadcn UI issues

--- nextjs-next-blog/src/app/page.tsx
+++ nextjs-next-blog/src/app/page.tsx
@@ -14,11 +14,14 @@ export default async function IndexPage() {
   const posts = await client.fetch<SanityDocument[]>(POSTS_QUERY, {}, options);
 
   return (
-    <main className="container mx-auto min-h-screen max-w-3xl p-8">
+    <main className="container mx-auto min-h-screen max-w-7xl py-8">
       <h1 className="text-4xl font-bold mb-8">Posts</h1>
-      <ul className="flex flex-col gap-y-4">
+      <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 gap-y-4">
         {posts.map((post) => (
-          <li className="hover:underline" key={post._id}>
+          <li
+            className="hover:underline border border-gray-600 p-5 w-full"
+            key={post._id}
+          >
             <Link href={`/${post.slug.current}`}>
               <h2 className="text-xl font-semibold">{post.title}</h2>
               <p>{new Date(post.publishedAt).toLocaleDateString()}</p>
